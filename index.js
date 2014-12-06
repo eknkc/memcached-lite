5454108fecd60aef77777609643625e6c546ed41
Ekin Koc <ekin@eknkc.com>
2014-12-07 00:02:08 +0200

    Minor

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var Client = require("./lib/client.js");
 
-module.exports = function(options) {
-  return new Client(options);
+module.exports = function(hosts, options) {
+  return new Client(hosts, options);
 }
 
 module.exports.Client = Client;
