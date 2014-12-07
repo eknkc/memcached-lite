c2b648fad26388da94c46d62f892955aafd84bd4
Ekin Koc <ekin@eknkc.com>
2014-12-07 13:51:53 +0200

    Benchmarks

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -1,21 +1,29 @@
-var c = require("../index")("localhost", { connectionsPerServer: 2 });
+var c = require("../index")("localhost", { connectionsPerServer: 1 });
 
 suite('basic', function() {
   before(function (next) {
     c.set('gettest1', "VALUE", 1000, next);
   });
 
-  set('concurrency', 20);
+  set('concurrency', 50);
 
   bench('get', function (next) {
     c.get('gettest1', next);
   });
 
   bench('set', function (next) {
-    c.set('settest1' + Math.random(), "VALUE", 100, next);
+    c.set('settest1', "VALUE", 100, next);
   });
 
   bench('incr', function (next) {
-    c.incr('settest1' + Math.random(), 1, 100, next);
+    c.incr('settest2', 1, 100, next);
+  });
+
+  bench('quiet set', function () {
+    c.set('settest1', "VALUE", 100);
+  });
+
+  bench('quiet incr', function () {
+    c.incr('settest2', 1, 100);
   });
 });
